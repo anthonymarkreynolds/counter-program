30c9230ad01d999b2fe4fd46d4ad005d53638982
anthonymarkreynolds <anthonymarkreynolds@outlook.com>
2023-01-29 23:26:57 +1000

    add view, more functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,37 +8,59 @@ import GHC.IO.Exception (ExitCode)
 clearConsole :: IO ExitCode
 clearConsole = system "clear"
 
-data GameState = GameState { score :: Int, turns :: Int, continue :: Bool }
+data GameState = GameState 
+    { score :: Int
+    , turns :: Int
+    , continue :: Bool }
+
 data MenuInput 
-    = Inc 
+    = Increment 
     | Reset 
     | Exit 
     deriving (Show, Enum, Bounded)
 
+menuInputs :: [MenuInput]
+menuInputs = [minBound :: MenuInput .. maxBound :: MenuInput]
+
 -- check if in range of menu items
 maybeMenuInput :: Int -> Maybe MenuInput
 maybeMenuInput i
-    | i > fromEnum (maxBound :: MenuInput) = Nothing
-    | i < fromEnum (minBound :: MenuInput) = Nothing
-    | otherwise  = Just ([minBound :: MenuInput .. maxBound :: MenuInput] !! i)
+    | n > fromEnum (maxBound :: MenuInput) = Nothing
+    | n < fromEnum (minBound :: MenuInput) = Nothing
+    | otherwise  = Just (menuInputs !! n)
+        where n = i - 1
 
-handleInput :: MenuInput -> GameState -> GameState
-handleInput input currentState = case input of
-    Inc -> currentState { score = score currentState + 1, turns = turns currentState + 1 }
-    Reset -> currentState { score = 0, turns = turns currentState }
-    Exit -> currentState { continue = False}
+startTurn :: GameState -> GameState
+startTurn currentState = currentState { turns = turns currentState + 1}
+
+handleMenu :: Maybe MenuInput -> GameState -> GameState
+handleMenu input currentState = startTurn $ case input of
+    Just Increment -> currentState { score = score currentState + 1}
+    Just Reset     -> currentState { score = 0}
+    Just Exit      -> currentState { continue = False}
+    Nothing        -> currentState 
+
+printMenuInput :: MenuInput -> IO ()
+printMenuInput input = putStrLn $ (show $ (fromEnum input)+1) ++ ". " ++ (show input)
+
+renderView :: GameState -> IO ()
+renderView currentState = do
+    _ <- clearConsole -- Clear the screen
+    putStrLn "Welcome to the counter program!"
+    let currentScore = score currentState
+    let currentTurn = turns currentState
+    mapM_ printMenuInput menuInputs
+    putStrLn ("The current score is: " ++ show currentScore)
+    putStrLn ("The current turn is: " ++ show currentTurn)
 
 program :: StateT GameState IO ()
 program = do
-    _ <- lift clearConsole
     currentState <- get
-    if not (continue currentState) then return ()
+    if not (continue currentState) then do
+        lift $ putStrLn "Goodbye"
+        return ()
     else do
-        let currentScore = score currentState
-        let currentTurns = turns currentState
-        lift $ putStrLn ("The current score is: " ++ show currentScore)
-        lift $ putStrLn ("The current turn is: " ++ show currentTurns)
+        lift $ renderView currentState
         input <- lift getLine
-        case (readMaybe input :: Maybe Int) >>= maybeMenuInput of
-            Just menuInput -> modify (handleInput menuInput) >> program
-            _  -> lift (putStrLn "Invalid input") >> program
+        menuInput <- lift $ pure $ (readMaybe input :: Maybe Int) >>= maybeMenuInput
+        modify (handleMenu menuInput) >> program
